865aaaaee2200363783c6eb70700e7aa23ac6672
czlowiekiglak <69763060+Cursinal@users.noreply.github.com>
2026-01-22 19:21:26 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const ZOOM_SETTINGS = {
 const PIN_SETTINGS = {
   mobile: {
     minZoomSize: 15,
-    maxZoomSize: 15,
+    maxZoomSize: 35,
     flagScale: 0.6
   },
   desktop: {
@@ -888,5 +888,6 @@ const App = () => {
 };
 
 export default App;
+
 
 
